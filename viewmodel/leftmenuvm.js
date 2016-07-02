01cb0999faeb082e846fb095af868bb2ed96a776
justme777 <ereke_enu@mail.ru>
2016-07-02 18:27:38 +0600

    adding create_widget_view.html

--- viewmodel/leftmenuvm.js
+++ viewmodel/leftmenuvm.js
@@ -1,5 +1,9 @@
 rssmailer.vm.leftmenu = rssmailer.vm.leftmenu || {};
-
-rssmailer.vm.leftmenu.showRegistrationView = function(){
+var model = rssmailer.vm.leftmenu;
+model.showRegistrationView = function(){
     $("#mainview").load("/view/registrationview.html");
+}
+
+model.showCreateWidgetView = function(){
+    $("#mainview").load("/view/create_widget_view.html");
 }
---
+++ viewmodel/widgetvm.js
@@ -0,0 +1,6 @@
+rssmailer.vm.widget = rssmailer.vm.widget||{};
+var m = rssmailer.vm.widget;
+
+m.createWidget = function(){
+    alert("on create widget");
+}
