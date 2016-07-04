bc0dbf735469d086bd2f035f4a5ce66e0c073097
justme777 <ereke_enu@mail.ru>
2016-07-04 14:14:48 +0600

    create widget

--- utils/startup.js
+++ utils/startup.js
@@ -2,9 +2,43 @@ var rssmailer = window.store||{};
 rssmailer.vm =rssmailer.vm||{};
 rssmailer.util = rssmailer.util||{}; 
 rssmailer.util.startup = rssmailer.util.startup || {};
+var model = rssmailer.util.startup;
 
-rssmailer.util.startup.loadViews=function(){
+model.loadConfig = function(){
+    $.getJSON("config.json", function (json) {
+        $.each(json, function (k, v) {
+            localStorage.setItem(k, v);
+        });
+    });
+}
+
+model.loadViews=function(){
     $("#sidebar-wrapper").load("/view/leftmenuview.html");
-    $("#mainview").load("/view/mainview.html");
-    
+    $("#mainview").load("/view/mainview.html");    
+}
+
+model.login = function(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var postData = { 'username': username, 'password': password };
+
+
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: localStorage.getItem("services_url") + "system/auth?token=0",
+        data: JSON.stringify(postData),
+        dataType: "json",
+        success: function (res) {
+            alert(res);
+            localStorage.setItem("token", res);
+            if (res == 401) {
+
+            }
+            else {
+                window.location = "index.html";
+            }
+        }
+    });
 }
--- viewmodel/widgetvm.js
+++ viewmodel/widgetvm.js
@@ -1,6 +1,35 @@
 rssmailer.vm.widget = rssmailer.vm.widget||{};
-var m = rssmailer.vm.widget;
+var model = rssmailer.vm.widget;
 
-m.createWidget = function(){
-    alert("on create widget");
+model.createWidget = function(){
+    var r = document.getElementById("txtRssLink").value;
+    var u = 1;
+    var postData = { 'rss': r, 'userId': u };
+    
+    
+     $.ajax({
+        type: "POST",
+        url: "http://rssmailer:8181/rest.php?name=createWidget",
+        data: { rss:r, userId:u},
+        dataType: 'html',
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+        },
+        success: function (result) {
+            alert(result);
+        }
+     });    
+    /*
+     $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "http://rssmailer:8181/rest.php?name=createWidget",
+        data: JSON.stringify(postData),
+        dataType: "json",
+        success: function (res) {
+            alert(res);
+        }
+    });
+    */
 }
+
