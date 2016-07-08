4d4e6c70aaf12fc6018ef71104edc14f7e3ce676
yerlan <ereke_enu@mail.ru>
2016-07-08 17:20:47 +0600

    left menu some changes

--- viewmodel/leftmenuvm.js
+++ viewmodel/leftmenuvm.js
@@ -1,8 +1,5 @@
 rssmailer.vm.leftmenu = rssmailer.vm.leftmenu || {};
 var model = rssmailer.vm.leftmenu;
-model.showRegistrationView = function(){
-    $("#mainview").load("/view/registrationview.html");
-}
 
 model.showCreateWidgetView = function(){
     $("#mainview").load("/view/create_widget_view.html");
