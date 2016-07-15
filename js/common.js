e21a7ed5066fde5ab4625f97848b4ef7ccc0e165
yerlan <ereke_enu@mail.ru>
2016-07-15 15:11:29 +0600

    get today date in format YYYY-mm-dd

--- js/common.js
+++ js/common.js
@@ -34,3 +34,19 @@ function getCookie(cname) {
     }
     return "";
 }
+
+//YYYY-mm-dd
+function getTodayDate(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+    return yyyy+"-"+mm+"-"+dd;
+}
